2aa950f384ee6dd3ee9d01cc0f3f887254546189
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2019-05-05 00:02:08 +0700

    Add requests limit

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require('express')
 const apicache = require('apicache')
+const axios = require('axios')
+const ddos = require('ddos')
 const app = express()
 const PORT = 3000
 
 let cache = apicache.middleware
 const menu = require('./menu')
 
+//Limit request balancer
+onDenial = err => {
+    console.warn(err);
+}
+
+const Ddos = new ddos({
+    burst: 30,
+    limit: 30,
+    maxCount: 60,
+    checkInterval: 5,
+    onDenial,
+    errormessage: "You have been blocked, Too many requests"
+})
+
 app.use(cache('5 minutes'))
+app.use(Ddos.express)
 
 app.get('/menu', (req, res) => {
     return res.status(200).json({
@@ -18,7 +35,7 @@ app.get('/menu', (req, res) => {
 app.get('/menu/:cat', (req, res) => {
     const cat = req.params.cat
     if (cat) {
-        if(menu[cat]) {
+        if (menu[cat]) {
             res.status(200).json({
                 success: true,
                 data: menu[cat]
