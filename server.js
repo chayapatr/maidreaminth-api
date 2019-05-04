3196d72cb28a16d8a4495e5d223018ebf2ff415d
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2019-05-04 23:07:19 +0700

    No more fs and util

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const express = require('express')
-const fs = require('fs')
-const util = require('util')
 
 const app = express()
 const PORT = 3000
 
-let rawMenu = fs.readFileSync('menu.json');
-let menu = JSON.parse(rawMenu);
+const menu = require('./menu')
+
 
 app.get('/menu', (req, res) => {
-    return res.status(200).send({
+    return res.status(200).json({
         success: true,
         data: menu
     })
@@ -19,18 +17,18 @@ app.get('/menu/:cat', (req, res) => {
     const cat = req.params.cat
     if (cat) {
         if(menu[cat]) {
-            res.status(200).send({
+            res.status(200).json({
                 success: true,
                 data: menu[cat]
             })
         } else {
-            res.status(404).send({
+            res.status(404).json({
                 success: false,
                 data: 'not found'
             })
         }
     } else {
-        res.status(400).send({
+        res.status(400).json({
             success: false,
             data: 'missing parameter'
         })
@@ -43,24 +41,23 @@ app.get('/menu/:cat/:food', (req, res) => {
     console.log(cat, food)
     if (food) {
         if (menu[cat][food]) {
-            res.status(200).send({
+            res.status(200).json({
                 success: true,
                 data: menu[cat][food]
             })
         } else {
-            res.status(404).send({
+            res.status(404).json({
                 success: false,
                 data: 'not found'
             })
         }
     } else {
-        res.status(400).send({
+        res.status(400).json({
             success: false,
             data: 'missing parameter'
         })
     }
 })
-
 app.listen(PORT, () => {
     console.log(`app run on port ${PORT}`);
 })
